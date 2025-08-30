1ba5904c07459d4cd4defe9430801cd573f8b4b9
Yunusov <abubakar.yunusov.47@mail.ru>
2025-08-30 16:04:37 +0300

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+// Назначить событие, виды назначения
+// 1) inline
+// 2) element.onclick()
+// 3) element.addEventListener()
+
+// Удалить событие
+// 1)element.removeEventListener()
+
+
+// что такое Event
+// Функция передающаяся в событие
